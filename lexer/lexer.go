94b1c4bcc378bc8215f7d7849dc273002794cbda
Falco <tomasetti@jacob.de>
2019-05-06 16:33:06 +0200

    use rune

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,13 +5,13 @@ import "jacob.de/gofact/token"
 // Lexer lexer object with functions
 type Lexer struct {
 	EdiFactMessage string
-	CurrentChar    string
+	CurrentChar    rune
 	CurrentSeq     string
 	ControlChars   control
 }
 
 func NewLexer(message string, controls string) *Lexer {
-	return &Lexer{EdiFactMessage: message, CurrentChar: "a", CurrentSeq: "", ControlChars: newControl("")}
+	return &Lexer{EdiFactMessage: message, CurrentChar: ' ', CurrentSeq: "", ControlChars: newControl("")}
 }
 
 func (e *Lexer) GetEdiTokens() []token.Token {
