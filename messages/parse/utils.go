0b29bd1948682b69cf05be529bfe53f4d6b99add
Falco <tomasetti@jacob.de>
2020-08-26 10:46:27 +0200

    use delimiter variable

--- messages/parse/utils.go
+++ messages/parse/utils.go
@@ -8,7 +8,7 @@ func handleIndicator(s string, indicator string, delimiter string) []string {
 	var indexToDelete []int
 	sTmp := strings.Split(s, indicator+delimiter)
 	for idx, sub := range sTmp {
-		tmp := strings.Split(sub, "+")
+		tmp := strings.Split(sub, delimiter)
 		elementsStr = append(elementsStr, tmp...)
 		if idx != len(sTmp)-1 {
 			relIndex = append(relIndex, len(elementsStr)-1)
